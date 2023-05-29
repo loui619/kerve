d87fcdb27f5ce8e5ff69163f8a288ced47bbf9cf
Nikhil Louis <nikhilouis619@gmail.com>
2023-05-29 10:15:20 +0530

    pagination fix
    pagination fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,26 +6,44 @@ import Pagination from "../Pagination/Pagination";
 const Home = ()=>{
     const [newsContent,setNewsContent] = useState();
     const [searchKey,setSearchKey] = useState();
+    const [pageNo,setPageNo] = useState(0);
+    const [searchEnable,setSerchEnable] = useState(false);
     const searchWord = (e)=>{
         setSearchKey(e.target.value)
     }
     useEffect(()=>{
-        const content = API.getItems().then((data) => {
+        API.getItems().then((data) => {
             setNewsContent(data?.response);
         })
     },[])
-    const searchNews = (pageNo)=>{
+    useEffect(()=>{
         API.getItembyKey(searchKey,pageNo).then((data)=>{
             setNewsContent(data?.response);
         })
-    }
+        
+    },[searchEnable])
+    const searchNews = (page)=>{
+        setSerchEnable(!searchEnable)
+        if(typeof page === 'object'){
+            if(pageNo==0){
+                setPageNo(1)
+            }
+            else{
+                setPageNo(pageNo)
+            }
+        }
+        else{
+            setPageNo(page);
+        }
+        
+     }
     return(
         <>
             <div className="home-container">
                 <div className="search-container">
                     
                     <input className="input-text" type="text" value={searchKey} onChange={searchWord}/>
-                    <button className="search-btn" onClick={searchNews}>Search</button>
+                    <button className="search-btn" onClick={()=>searchNews()}>Search</button>
                 </div>
                 <div className="news-container">
                     <News newsfeeds={newsContent}/>
--- src/components/Utils/Api.js
+++ src/components/Utils/Api.js
@@ -3,7 +3,7 @@ export default {
       return fetch('http://content.guardianapis.com/search?api-key=test&amp;show-fields=thumbnail,headline&amp;page=1&amp;page-size=10')
         .then(res => res.json());
     },
-    getItembyKey: (searchKey,pageNo=1) =>{
+    getItembyKey: (searchKey,pageNo="1") =>{
         return fetch(`http://content.guardianapis.com/search?api-key=test&amp;q=${searchKey}&amp;show- fields=thumbnail,headline&amp;show-tags=keyword&amp;page=${pageNo}&amp;page-size=10`)
         .then(res => res.json());
     }
